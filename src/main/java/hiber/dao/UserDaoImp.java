c8d3102c65542f5f1de90e421b64783551c51009
DaryaTaupinets <dansy1987@gmail.com>
2020-05-06 00:40:29 +0300

    PreProject 2.2.1 version 1.1

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -2,7 +2,8 @@ package hiber.dao;
 
 import hiber.model.Car;
 import hiber.model.User;
-import org.hibernate.*;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
